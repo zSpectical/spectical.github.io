cdaf362207ea9910e53086060db114155c37f3a2
zSpectical <kian.shirvani@outlook.com>
2023-08-29 23:26:44 -0700

    Update email.js

--- email.js
+++ email.js
@@ -1,11 +1,3 @@
 function sendEmail() {
-    let yourName = document.getElementById("yourName").value;
-    let yourEmail = document.getElementById("yourEmail").value;
-    let yourMessage = document.getElementById("yourMessage").value;
-
-    let link = "mailto:kian.shirvani@outlook.com" +
-               "?subject=" + encodeURIComponent("Message from " + yourName) +
-               "&body=" + encodeURIComponent(yourMessage + "\n\n" + "From: " + yourEmail);
-
-    window.location.href = link;
+    alert("Function called!");
 }
